0d72b97bd8ec063b4143a2f7679e84806c985bf2
Tomas <tomas@Tomas-MacBook.local>
2023-03-25 16:51:29 +0800

    fix: changed WA folder location in WAPage.js

--- src/content/WApage/WApage.js
+++ src/content/WApage/WApage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const WApage = () => {
   return (
       <div class="whatsapp-trimmer">
-        <iframe title="WhatsApp trimmer" src="/_WA/index.html" />
+        <iframe title="WhatsApp trimmer" src="/__WA/index.html" />
       </div>
   );
 }
