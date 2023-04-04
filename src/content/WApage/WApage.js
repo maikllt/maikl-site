1bc0a4b58f7bb265c45a9f861a8300921eff1c98
Tomas <tomas@Tomas-MacBook.local>
2023-04-04 12:14:41 +0800

    fix: test variables in VERCEL

--- src/content/WApage/WApage.js
+++ src/content/WApage/WApage.js
@@ -4,7 +4,7 @@ const WApage = () => {
   return (
     <div className="whatsapp-trimmer">
       <iframe id="whatsapp-trimmer" title="WhatsApp trimmer" src={process.env.REACT_APP_WA_TRIMMER_URL} />
-    </div>
+    </div> 
   );
 }
 
