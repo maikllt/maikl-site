dd053933df9003ee80be1e67128846cc5faae29b
Tomas <hi@maikl.lt>
2023-04-05 14:21:20 +0800

    feat: IKIGAI page layout

--- src/content/IKIGAIpage/IKIGAIpage.js
+++ src/content/IKIGAIpage/IKIGAIpage.js
@@ -1,28 +1,97 @@
-import React from 'react';
+import React, { useState, setOpen } from 'react';
+import {
+  Button,
+  Grid,
+  Column,
+  TextArea,
+  Modal,
+  Loading
+} from '@carbon/react';
+import { Link } from '@carbon/react/icons';
+import ReactDOM from 'react-dom';
 
 const IKIGAIpage = () => {
   return (
-    <div>
-      <h1>Ikigai Model</h1>
+    <Grid className="ikigai-page" fullWidth>
+      <Column lg={6} md={4} sm={4} className="ikigai-page-desription">
+        <h2 className="ikigai-page__heading">What is <strong>IKIGAI</strong>?</h2>
+        <p className="ikigai-page__p">
+          "Japanese concept referring to something that gives a person a sense of <strong>purpose</strong>,
+          a <strong>reason for living</strong>."
+          <a href="https://en.wikipedia.org/wiki/Ikigai" target="_blank" rel="noopener noreferrer">
+            <Link size="16" />
+          </a>
+        </p>
+        <img
+          className="ikigai-picture"
+          src="http://www.djkn.kemenkeu.go.id/files/images/2020/09/Diagram_Ikigai1.jpg"
+          alt="IKIGAI diagram"
+        />
+      </Column>
+      <Column lg={8} md={4} sm={4} className="ikigai-page-content">
+        <TextArea
+          enableCounter
+          id="text-area-1"
+          labelText="What you LOVE"
+          maxCount={300}
+          rows="3"
+        />
+        <TextArea
+          enableCounter
+          id="text-area-2"
+          labelText="What the world NEEDS"
+          maxCount={300}
+          rows="3"
+        />
+        <TextArea
+          enableCounter
+          id="text-area-3"
+          labelText="What you can be PAID FOR"
+          maxCount={300}
+          rows="3"
+        />
+        <TextArea
+          enableCounter
+          id="text-area-4"
+          labelText="What are you GOOD AT"
+          maxCount={300}
+          rows="3"
+        />
+        <ModalStateManager
+          renderLauncher={({ setOpen }) => (
+            <Button kind="primary" onClick={() => setOpen(true)}>Give my IKIGAI</Button>
+          )}>
+          {({ open, setOpen }) => (
+            <Modal
+              modalHeading="Your IKIGAI"
+              primaryButtonText="Try again"
+              open={open}
+              onRequestClose={() => setOpen(false)}>
+              <p>SORRY! work still in progress... </p>
+              <Loading className="ikigai-loeader" withOverlay={false} />;
+            </Modal>
+          )}
+        </ModalStateManager>
+      </Column>
+    </Grid>
+  );
+};
 
-      <div className="container">
-        <div className="circle">
-          <h2>What you love</h2>
-        </div>
-
-        <div className="circle">
-          <h2>What you're good at</h2>
-        </div>
-
-        <div className="circle">
-          <h2>What the world needs</h2>
-        </div>
-
-        <div className="circle">
-          <h2>What you can be paid for</h2>
-        </div>
-      </div>
-    </div>
+const ModalStateManager = ({
+  renderLauncher: LauncherContent,
+  children: ModalContent,
+}) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      {!ModalContent || typeof document === 'undefined'
+        ? null
+        : ReactDOM.createPortal(
+          <ModalContent open={open} setOpen={setOpen} />,
+          document.body
+        )}
+      {LauncherContent && <LauncherContent open={open} setOpen={setOpen} />}
+    </>
   );
 };
 
